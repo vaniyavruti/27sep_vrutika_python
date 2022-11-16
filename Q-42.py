0f40ddaed934b93b0ea7a2072d11111d7a4df995
vaniyavrutu <115069945+vaniyavrutu@users.noreply.github.com>
2022-11-16 13:50:34 +0530

    Delete Q-42.py

--- Q-42.py
+++
@@ -1,9 +0,0 @@
-""" Write a Python function that checks whether a passed string is 
-palindrome or not  """
-s = input("Enter a string")
-revstr = (s[::-1])
-if revstr == s:
-    print("palindrom")
-else:
-    print(("not palindrom"))
-    
