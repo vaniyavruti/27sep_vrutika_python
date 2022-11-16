3f212333d335c0fcf75fe8fc76356ee12a34d0e6
vaniyavrutu <115069945+vaniyavrutu@users.noreply.github.com>
2022-11-16 13:51:44 +0530

    Delete Q-46.py

--- Q-46.py
+++
@@ -1,6 +0,0 @@
-"""  Write a Python program to calculate the area of a parallelogram """
-
-base = float(input('Length of base: '))
-height = float(input('Measurement of height: '))
-area = base * height
-print("Area is: ", area)
