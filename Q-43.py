6bdbc8dac9db6d384051526705b99516bdd72ea7
vaniyavrutu <115069945+vaniyavrutu@users.noreply.github.com>
2022-11-16 13:51:00 +0530

    Delete Q-43.py

--- Q-43.py
+++
@@ -1,6 +0,0 @@
-""" Write a Python program to read a random line from a file. """
-import random
-def random_line(fname):
-    lines = open(fname).read().splitlines()
-    return random.choice(lines)
-print(random_line('test.txt'))
